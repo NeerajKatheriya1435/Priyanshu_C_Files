2c710cd6c3735d54e45037dd8eff700db097f9e3
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-04-28 18:01:12 +0530

    operator started

--- day1.c
+++ day1.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello word");
+    // printf("Hello word");
+
+    // int a=8;
+    // int b=7;
+    // printf("Hello the sum is: %d",(a+b));
+
+    // float inp1=5.6;
+    // float inp2=7.67;
+    // char s1='s';
+    // printf("The char is: %c",s1);
+
+    // int raman=78
+    // int ramankumar=78
+    // int ramankumar12345678=78
+    // int ramankudgfd46456fdg5465mar12345678=78
+    // int _ramankudgfd4_6456fdg5465mar12345678=78;
+    // int tuyifgre89iu=5765;
+    // int tuyifgre89iu=5765;
+
+    // int rohan=78;
+    // rohan=45;
+    // int ROHAN=56;
+    // int ROHAn=90;
+    // int ROHan=90;
+    // printf("%d\n",rohan);
+    // printf("%d\n",ROHAN);
+    // int inp1=89;
+    // int input_Number_One=78
+
+    // printf("%f",inp1+inp2);
+
+    // Name String Value-->"Rohan","Ram"
+    // Name Int Value-->34, -78
+    // Name Float Value-->3.4, -7.8,867.677865 
+    // Name Double Value-->3.4, -7.8,867.677767865
+    // Name Char Value-->"C",'r','9','*'
+    printf("%d\n",8+8);
+    printf("%d\n",8-7);
+    printf("%d\n",8*7);
+    printf("%d\n",8/4);
     return 0;
 }
