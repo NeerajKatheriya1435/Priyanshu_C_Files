b95208dd5122261138de3c5a1a4e9132472e8c3b
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-04-28 18:49:56 +0530

    Operators and question

---
+++ day2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    // int a=7;
+    // int b=3;
+    // float a=7;
+    // float b=3;
+    // printf("Remainder is: %d",a%b);
+    // printf("Remainder is: %f",a/b);
+
+    // Relational Operators
+
+    // int a=7;
+    // int b=5;
+
+    // printf("%d",a==b);
+    // printf("%d\n",a>b); //1
+    // printf("%d\n",a>=b); //1
+    // printf("%d\n",a<b); // 0
+    // printf("%d\n",a<=b); // 0
+    // printf("%d\n",a!=b); //1
+    // printf("%d\n",a==b);  // 0
+
+    //C.  Logical operators
+
+    // printf("%d\n",(17>9) && (19>12) && (3<8));
+    // printf("%d\n",(7>19) && (19>12) && (3<8));
+
+    // printf("%d\n",(17>9) || (19>12) || (3<8));
+    // printf("%d\n",(7>19) || (1>12) || (13<8));
+
+    // printf("%d\n",!((17>9) || (19>12) || (3<8)));
+    // printf("%d\n",(7>19) || (1>12) || (13<8));
+
+    // D. Assignment Operators
+    // int a=8;
+    // a=a+6;
+    // a+=6;
+    // a-=4;
+    // a/=2;
+    // a*=5;
+    // a=a*5;
+    // printf("%d\n",a);
+
+    // int a=6;
+    // ++a;
+    // a++;
+    // a+=1;
+    // printf("%d\n",a);
+    
+    // int a=6;
+    // ++a;
+    // printf("%d\n",++a); // Pre increment
+    // printf("%d\n",a++);
+
+    // printf("%d\n",++a); //7
+    // printf("%d\n",a); // 7
+    // printf("%d\n",a++); // 7
+    // printf("%d\n",++a); //9
+    // printf("%d\n",a);//9
+    // printf("%d\n",a--); //9
+    // printf("%d\n",a);//8
+    // printf("%d\n",--a);//7
+    // printf("%d\n",++a);//8
+    // printf("%d\n",a++);//8
+    // printf("%d\n",a);//9
+
+    // F. Bitwise Operators
+    // printf("%d\n",(7&5));
+    // printf("%d\n",(7|5));
+
+    // G. Conditional (Ternary) Operator
+
+    // int a=15>10?5:34;
+    // printf("%d\n",a);
+
+    // H. Special Operators
+
+    // double a=1;
+    // printf("%d\n",sizeof(a));
+
+    double a=1;
+    
+    printf("%d\n",a);
+
+    return 0;
+}
