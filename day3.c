8ffa7eef3f1a686b97a758847ed54689ceb6816c
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-04-30 19:01:21 +0530

    switch case and input

---
+++ day3.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // int a=6;
+    // printf("The num is: %d\n",a);
+    // printf("The address of num is: %x\n",&a);
+
+    // float b=6;
+    // printf("The num is: %.2f\n",b);
+
+    // double c=6.78;
+    // printf("The num is: %lf\n",c);
+
+    // printf("Hello \\neeraj i \\\" am good and \ni am good");
+    // printf("Hello \\' neeraj \0 i\\t\tam good");
+
+    // int age=89;
+
+    // if(age<0 || age > 120){
+    //     printf("Invalid Age");
+    // }
+    // else if(age>0 && age<18){
+    //     printf("You are child");
+    // }
+    // else if(age>18 && age<80){
+    //     printf("You can drive the car");
+    // }
+    // else{
+    //     printf("You are overaged");
+    // }
+
+    // int age=25;
+
+    // if(age>18){
+    //     printf("Good Age\n");
+    //     if(age==35){
+    //         printf("Congrats you can be president");
+    //     }else{
+    //         printf("You are not good");
+    //     }
+    // }
+
+    // int marks = 6;
+
+    // if (marks >= 90) {
+    // printf("Grade A");
+    // } else if (marks >= 75) {
+    // printf("Grade B");
+    // } else if (marks >= 50) {
+    // printf("Grade C");
+    // } else {
+    //     printf("Fail");
+    // }
+
+    // int leap=2020;
+    // if((leap%4==0 && leap%100!=0) || leap%400==0){
+    //     printf("Is Leap Year");
+    // }else{
+    //     printf("Not Leap Year");
+    // }
+
+
+
+    return 0;
+}
---
+++ day4.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    // int value='+';
+    // int a=8;
+    // int b=4;
+
+    // switch (value){
+    //     case '+':
+    //         // printf("Sunday");
+    //         printf("The sum is: %d\n",a+b);
+    //         break;
+    //     case 2:
+    //         printf("Monday");
+    //         break;
+    //     case 3:
+    //         printf("Tuesday");
+    //         break;
+    //     case 4:
+    //         printf("Wednesday");
+    //         break;
+    //     case 5:
+    //         printf("Thursday");
+    //         break;
+    //     default:
+    //         printf("Please input 1 to 7");
+    // }
+
+    // int a;
+
+    // printf("Enter the number: \n");
+    // scanf("%d",&a); // for taking the value
+    // scanf("%d",&a); // for taking the value
+
+    // printf("The value of a is: %d",a);
+
+    // int num1;
+    // char favChar;
+    // float newFloat;
+
+    // scanf("%c",&favChar);
+    // scanf("%d",&num1);
+    // scanf("%f",&newFloat);
+
+    // printf("The favChar is: %c\n",favChar);
+    // printf("The num1 is: %d\n",num1);
+    // printf("The newFloat is: %f\n",newFloat);
+
+    int num1=5;
+    int num2=8;
+
+    // printf("The average is: %d",(num1+num2)/2);
+    printf("The average is: %f",((float)num1+num2)/2);
+
+    int x = 10;
+    float y = x; // int automatically converted to float
+    printf("%d", (int)y);
+
+    return 0;
+}
+
+
+    
+
