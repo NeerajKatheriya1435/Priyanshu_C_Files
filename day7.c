3133f3e19306bd90d3b61de8ee753e7a3cdd6101
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-04 18:48:03 +0530

    functions and parameters

---
+++ day7.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+int main()
+{
+// start:
+//     printf("Hello World!!!\n");
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         if(i==3){
+//             goto end;
+//         }
+//         printf("%d\n",i);
+//     }
+
+
+// middle:
+//     printf("Loop Terminated Successfully\n");
+
+// end:
+//     printf("Program Finished\n");
+
+// for (int i = 1; i <= 3; i++) {
+//     for (int j = 1; j <= 3; j++) {
+//         if (i == 2 && j == 2) {
+//             goto end; // exit both loops
+//         }
+//         printf("%d %d\n", i, j);
+//         }
+//     }
+
+// end:
+// printf("Exited from nested loops.");
+
+    // Simple addition Function
+
+    // void addTwoNum(int a,int b){ // Copy Parameters
+    //     printf("The sum is: %d\n",(a+b));
+    // };
+
+    // addTwoNum(5,4); // Actual Arguments
+    // addTwoNum(3,2);
+    // addTwoNum(6,8);
+    // addTwoNum(2,9);
+
+    // void greet(){
+    //     printf("Hello Good Morning");
+    // }
+
+    // greet();
+
+    // float average(float a,float b){
+    //     return (a+b)/2;
+    // }
+
+    // float result=average(4,7);
+    // printf("%f",result);
+
+    // 1,2,3,4,5
+    // 15
+
+    // int sumFunc(int num1){
+    //     int sum=0;
+    //     for (int i = 1; i <=num1; i++)
+    //     {
+    //         sum=sum+i;
+    //     }
+    //     return sum;
+    // }
+
+    // int result=sumFunc(100);
+
+    // printf("The sum is: %d",result);
+
+    return 0;
+}
