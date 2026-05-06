115dcffff941787025bf26b8994495670cd44bc4
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-06 18:58:00 +0530

    Local Global Variables

---
+++ day9.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+// int a=6;
+
+void addTwoNum(int num1,int num2){
+    // int a=5;
+    // a=27;
+    num1=9;
+    printf("The sum is: %d\n",(num1+num2)); //15
+    // printf("%d\n",a);
+}
+
+int main()
+{
+
+    int a=7;
+    
+    addTwoNum(a,6);
+    
+    printf("%d\n",a);
+
+    return 0;
+}
