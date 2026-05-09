daef1f6fe3f03b504e87cd6cc2b6c9298a9609f2
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-09 15:37:36 +0530

    arrays started and loop through array

---
+++ day10.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // int marks={3,5,7,8};
+
+    // int arr[10];
+
+    // arr[0]=56;
+    // arr[1]=23;
+    // arr[2]=78;
+
+    // printf("The num %d\n",arr[1]);
+
+    // for(int i=0;i<3;i++){
+    //     printf("%d\n",arr[i]);
+    // }
+
+    // int arr[2][3];
+
+    // arr[0][0]=56;
+    // arr[0][1]=16;
+    // arr[0][2]=56;
+
+    // printf("The num is: %d\n",arr[0][1]);
+
+    // for(int i=0;i<2;i++){
+    //     for(int j=0;j<3;j++){
+    //         printf("Enter the value at: %d,%d\n",i,j);
+    //         scanf("%d",&arr[i][j]);
+    //     }
+    // }
+    // for(int i=0;i<2;i++){
+    //     for(int j=0;j<3;j++){
+    //         printf("The value at %d,%d is %d\n",i,j,arr[i][j]);
+    //     }
+    // }
+    // int a=8;
+    // int* ptr=&a;
+
+    // printf("The value in a %d\n",a);
+    // printf("The value in ptr %d\n",ptr);
+    // printf("The adreess of a %d\n",&a);
+    // // printf("The adreess of ptr %d\n",&ptr);
+    // printf("The adreess of ptr %d\n",*ptr);
+
+    // int arr[3] = {10, 20, 30};
+
+    // printf("The num is: %d\n",*arr);
+    // printf("The num is: %d\n",*(arr+1));
+    // printf("The num is: %d\n",*(arr+2));
+
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // int *ptr = arr;
+    // for(int i = 0; i < 5; i++) {
+    //     printf("%d ", *(ptr + i));
+    // }
+
+
+    return 0;
+}
